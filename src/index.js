b9662b28dfc740fe621a5cf2c3c0b2b7fe7f412a
Rdavid1991 <42180920+Rdavid1991@users.noreply.github.com>
2022-06-10 22:51:14 -0500

    Cambio a typescript

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 
-console.log("esta vaina");
-
 import App from "./App";
 
 document.addEventListener("DOMContentLoaded", () => {
