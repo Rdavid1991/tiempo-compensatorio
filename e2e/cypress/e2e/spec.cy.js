6bd0ba04b6ca7cfc18972cf336f9455947e92c12
Ramiro Centeno <rdavid3.1991@gmail.com>
2022-06-15 15:53:46 -0500

    🔨Creacion de funcionario

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -1,5 +1,36 @@
+/// <reference types="Cypress"/>
+
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit("http://localhost:3000")
+  })
+
+  it("Crear funcionario", () => {
+    cy.get("i.fa-plus").parent().click()
+
+    cy.get("#addFunctionary").then((modal) => {
+      cy.get(modal).should("be.visible")
+      cy.get(modal).should("have.class", "show")
+
+      //cy.get(modal).get("modal-title").should("have.text", "Agregar funcionario")
+
+      cy.get(modal).get('#name').type("Cypress")
+
+      const date = new Date();
+
+      let ye = date.toLocaleDateString([],{ year: 'numeric' }) 
+      let mo = date.toLocaleDateString([],{ month: '2-digit' }) 
+      let da = date.toLocaleDateString([],{ day: '2-digit' }) 
+
+      cy.get(modal).get('#day').type(`${ye}-${mo}-${da}`)
+      cy.get('#addEmployForm > .modal-footer > button[type="submit"]').click()
+    })
+
+    cy.get("#addFunctionary").then((modal) => {
+      cy.get(modal).should("not.be.visible")
+      cy.get(modal).should("not.have.class", "show")
+
+      //cy.get(modal).get("modal-title").should("have.text", "Agregar funcionario"
+    })
   })
 })
